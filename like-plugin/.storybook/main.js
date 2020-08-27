e63a62e8372b1895806b38cd2aea0d7901d2da59
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-08-28 00:55:36 +0900

    chore: zero-config typescript setup for storybook
    ref. https://storybook.js.org/docs/react/configure/typescript

--- like-plugin/.storybook/main.js
+++ like-plugin/.storybook/main.js
@@ -1,19 +1,14 @@
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve("ts-loader"),
-        },
-        // Optional
-        // {
-        //   loader: require.resolve("react-docgen-typescript-loader"),
-        // },
-      ],
-    });
-    config.resolve.extensions.push(".ts", ".tsx");
-    return config;
+
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
   },
 };
