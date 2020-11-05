5f1e5e125a38be6e7b609c164ad504f954003615
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-11-05 16:44:09 +0900

    build: copy zaki-yama/typescript-kintone-customize-template

---
+++ google-calendar-integration/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    config: "./src/main.tsx"
+  },
+  output: {
+    path: path.join(__dirname, "dist", "js"),
+    filename: "bundle.js"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  }
+};
