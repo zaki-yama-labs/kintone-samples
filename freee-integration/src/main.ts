d509e9082a78272ad096e3656ee96a04478136d4
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-12-11 17:13:38 +0900

    freee > queryをつける必要があった

--- freee-integration/src/main.ts
+++ freee-integration/src/main.ts
@@ -6,21 +6,11 @@ type KintoneEvent = {
 };
 
 (function () {
-  // クライアントID
-  const client_id = process.env.CLIENT_ID;
-  // カレンダーID
-  const calendar_id = process.env.CALENDAR_ID;
-  // 認証用URL（読み取り／更新）
-  const scope = "https://www.googleapis.com/auth/calendar";
-  // Discovery Docs
-  const discovery_docs = [
-    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-  ];
-
   // 保存済みのfreee認証がない場合、レコードの新規作成に移動する
   kintone.events.on(["app.record.index.show"], async (event: KintoneEvent) => {
     const params = {
       app: kintone.app.getId(),
+      query: "作成者 in (LOGINUSER())",
     };
     const resp = await kintone.api("/k/v1/records", "GET", params);
     if (resp.records.length !== 1) {
